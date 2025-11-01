efc871c92d034c2e348fe015cf504e52d7811467
jalalannan <jalalannan4@gmail.com>
2025-11-01 10:56:21 +0200

    remove the deleted

--- src/app/api/students/search/route.ts
+++ src/app/api/students/search/route.ts
@@ -39,6 +39,11 @@ export async function POST(request: Request) {
 
     if (f.verified !== undefined) {
         filtersParts.push(`verified:"${f.verified === '1' ? '1' : '0'}"`);
+        if(f.verified === '1') {
+          filtersParts.push(`is_deleted:0`);
+        } else {
+          filtersParts.push(`is_deleted:1`);
+        }
     }
     if (f.is_banned !== undefined) {
     filtersParts.push(`is_banned:"${f.is_banned === '1' ? '1' : '0'}"`);
